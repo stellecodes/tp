94a3ddb0ee43f356dc5a9dd4f1b7fbd772dd8fe6
Stelle <stellelim05@gmail.com>
2025-10-09 01:46:44 +0800

    Add test method to TagContainsKeywordsPredicate

--- src/main/java/seedu/address/model/person/TagContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/TagContainsKeywordsPredicate.java
@@ -4,7 +4,11 @@ import java.util.List;
 import java.util.function.Predicate;
 
 import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.model.tag.Tag;
 
+/**
+ * Tests that a {@code Person}'s tags matches any of the keywords given.
+ */
 public class TagContainsKeywordsPredicate implements Predicate<Person> {
     private final List<String> keywords;
 
@@ -14,7 +18,10 @@ public class TagContainsKeywordsPredicate implements Predicate<Person> {
 
     @Override
     public boolean test(Person person) {
-        return true;
+        return person.getTags().stream()
+                .map(Tag::getTagName)
+                .anyMatch(tagName -> keywords.stream()
+                        .anyMatch(keyword -> tagName.equalsIgnoreCase(keyword)));
     }
 
     @Override
