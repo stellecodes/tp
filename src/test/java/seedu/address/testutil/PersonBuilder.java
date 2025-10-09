85167df9174a191469100e785c8892aa43a1b910
Xiao-Congchen <xiaocongchen.xcc@gmail.com>
2025-10-09 18:41:37 +0800

    Remove DEFAULT_REMARK as it is optional

--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -21,7 +21,6 @@ public class PersonBuilder {
     public static final String DEFAULT_PHONE = "85355255";
     public static final String DEFAULT_EMAIL = "amy@gmail.com";
     public static final String DEFAULT_ADDRESS = "123, Jurong West Ave 6, #08-111";
-    public static final String DEFAULT_REMARK = "Hates Math";
 
     private Name name;
     private Phone phone;
@@ -38,7 +37,6 @@ public class PersonBuilder {
         phone = new Phone(DEFAULT_PHONE);
         email = new Email(DEFAULT_EMAIL);
         address = new Address(DEFAULT_ADDRESS);
-        remark = new Remark(DEFAULT_REMARK);
         tags = new HashSet<>();
     }
 
