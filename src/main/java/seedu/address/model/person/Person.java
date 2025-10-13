5c319622eb60a29cdead4f440ac334378ca3eda5
Xiao-Congchen <xiaocongchen.xcc@gmail.com>
2025-10-13 20:46:53 +0800

    Remove tag from superclass Person

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -24,19 +24,17 @@ public class Person {
     // Data fields
     private final Address address;
     private final Remark remark; // optional field
-    private final Set<Tag> tags = new HashSet<>();
 
     /**
      * Every field must be present and not null except remark.
      */
     public Person(Name name, Phone phone, Email email, Address address, Remark remark, Set<Tag> tags) {
-        requireAllNonNull(name, phone, email, address, tags);
+        requireAllNonNull(name, phone, email, address);
         this.name = name;
         this.phone = phone;
         this.email = email;
         this.address = address;
         this.remark = remark;
-        this.tags.addAll(tags);
     }
 
     public Name getName() {
@@ -57,14 +55,6 @@ public class Person {
 
     public Remark getRemark() {
         return remark;
-    }
-
-    /**
-     * Returns an immutable tag set, which throws {@code UnsupportedOperationException}
-     * if modification is attempted.
-     */
-    public Set<Tag> getTags() {
-        return Collections.unmodifiableSet(tags);
     }
 
     /**
@@ -100,14 +90,13 @@ public class Person {
                 && phone.equals(otherPerson.phone)
                 && email.equals(otherPerson.email)
                 && address.equals(otherPerson.address)
-                && remark.equals(otherPerson.remark)
-                && tags.equals(otherPerson.tags);
+                && remark.equals(otherPerson.remark);
     }
 
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash(name, phone, email, address, remark, tags);
+        return Objects.hash(name, phone, email, address, remark);
     }
 
     @Override
@@ -118,7 +107,6 @@ public class Person {
                 .add("email", email)
                 .add("address", address)
                 .add("remark", remark)
-                .add("tags", tags)
                 .toString();
     }
 
