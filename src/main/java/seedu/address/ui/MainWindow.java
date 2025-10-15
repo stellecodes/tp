3f49d7a180c4f3c9da30edb16d4c205388a3f550
Stelle <stellelim05@gmail.com>
2025-10-15 20:50:19 +0800

    Add basic tag list UI

--- src/main/java/seedu/address/ui/MainWindow.java
+++ src/main/java/seedu/address/ui/MainWindow.java
@@ -32,6 +32,7 @@ public class MainWindow extends UiPart<Stage> {
 
     // Independent Ui parts residing in this Ui container
     private PersonListPanel personListPanel;
+    private TagListPanel tagListPanel;
     private ResultDisplay resultDisplay;
     private HelpWindow helpWindow;
 
@@ -43,6 +44,9 @@ public class MainWindow extends UiPart<Stage> {
 
     @FXML
     private StackPane personListPanelPlaceholder;
+
+    @FXML
+    private StackPane tagListPanelPlaceholder;
 
     @FXML
     private StackPane resultDisplayPlaceholder;
@@ -112,6 +116,9 @@ public class MainWindow extends UiPart<Stage> {
     void fillInnerParts() {
         personListPanel = new PersonListPanel(logic.getFilteredPersonList(), logic);
         personListPanelPlaceholder.getChildren().add(personListPanel.getRoot());
+
+        tagListPanel = new TagListPanel(logic.getAddressBook().getTagList());
+        tagListPanelPlaceholder.getChildren().add(tagListPanel.getRoot());
 
         resultDisplay = new ResultDisplay();
         resultDisplayPlaceholder.getChildren().add(resultDisplay.getRoot());
---
+++ src/main/java/seedu/address/ui/TagListPanel.java
@@ -0,0 +1,42 @@
+package seedu.address.ui;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.logging.Logger;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.Region;
+import seedu.address.commons.core.LogsCenter;
+import seedu.address.logic.Logic;
+import seedu.address.model.person.Person;
+import seedu.address.model.tag.Tag;
+import seedu.address.model.tag.UniqueTagList;
+
+/**
+ * Panel containing the list of unique tags.
+ */
+public class TagListPanel extends UiPart<Region> {
+    private static final String FXML = "TagListPanel.fxml";
+    private final Logger logger = LogsCenter.getLogger(TagListPanel.class);
+
+    @FXML
+    private ListView<Tag> tagListView;
+
+    /**
+     * Creates a {@code TagListPanel} with the given {@code UniqueTagList}.
+     */
+    public TagListPanel(UniqueTagList uniqueTagList) {
+        super(FXML);
+        Set<Tag> tagSet = uniqueTagList.getTags();
+        List<Tag> sortedTags = new ArrayList<>(tagSet);
+
+        // Wrap as observable and show
+        ObservableList<Tag> observableTags = FXCollections.observableArrayList(sortedTags);
+        tagListView.setItems(observableTags);
+    }
+}
