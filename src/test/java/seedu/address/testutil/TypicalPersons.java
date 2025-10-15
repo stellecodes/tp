437ece8e53da69b3b0a54624d88ea7985215461a
Xiao-Congchen <xiaocongchen.xcc@gmail.com>
2025-10-15 13:33:04 +0800

    Refactor with new build parent/student method

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -30,36 +30,36 @@ public class TypicalPersons {
     public static final Student ALICE = (Student) new PersonBuilder().withName("Alice Pauline")
             .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@example.com")
             .withPhone("94351253")
-            .withTags("Science").build();
+            .withTags("Science").buildStudent();
     public static final Student BENSON = (Student) new PersonBuilder().withName("Benson Meier")
             .withAddress("311, Clementi Ave 2, #02-25")
             .withEmail("johnd@example.com").withPhone("98765432")
-            .withTags("Science", "Math").build();
+            .withTags("Science", "Math").buildStudent();
     public static final Parent CARL = (Parent) new PersonBuilder().withName("Carl Kurz").withPhone("95352563")
-            .withEmail("heinz@example.com").withAddress("wall street").build();
+            .withEmail("heinz@example.com").withAddress("wall street").buildParent();
     public static final Parent DANIEL = (Parent) new PersonBuilder().withName("Daniel Meier").withPhone("87652533")
-            .withEmail("cornelia@example.com").withAddress("10th street").withTags("friends").build();
+            .withEmail("cornelia@example.com").withAddress("10th street").withTags("friends").buildParent();
     public static final Parent ELLE = (Parent) new PersonBuilder().withName("Elle Meyer").withPhone("9482224")
-            .withEmail("werner@example.com").withAddress("michegan ave").build();
+            .withEmail("werner@example.com").withAddress("michegan ave").buildParent();
     public static final Parent FIONA = (Parent) new PersonBuilder().withName("Fiona Kunz").withPhone("9482427")
-            .withEmail("lydia@example.com").withAddress("little tokyo").build();
+            .withEmail("lydia@example.com").withAddress("little tokyo").buildParent();
     public static final Parent GEORGE = (Parent) new PersonBuilder().withName("George Best").withPhone("9482442")
-            .withEmail("anna@example.com").withAddress("4th street").build();
+            .withEmail("anna@example.com").withAddress("4th street").buildParent();
 
     // Manually added
     public static final Student HOON = (Student) new PersonBuilder().withName("Hoon Meier").withPhone("8482424")
-            .withEmail("stefan@example.com").withAddress("little india").withTags("Science").build();
+            .withEmail("stefan@example.com").withAddress("little india").withTags("Science").buildStudent();
     public static final Parent IDA = (Parent) new PersonBuilder().withName("Ida Mueller").withPhone("8482131")
-            .withEmail("hans@example.com").withAddress("chicago ave").build();
+            .withEmail("hans@example.com").withAddress("chicago ave").buildParent();
 
     // Manually added - Person's details found in {@code CommandTestUtil}
     public static final Parent AMY = (Parent) new PersonBuilder().withName(VALID_NAME_AMY).withPhone(VALID_PHONE_AMY)
             .withEmail(VALID_EMAIL_AMY).withAddress(VALID_ADDRESS_AMY)
-            .withRemark(VALID_REMARK_AMY).build();
+            .withRemark(VALID_REMARK_AMY).buildParent();
     public static final Student BOB = (Student) new PersonBuilder().withName(VALID_NAME_BOB).withPhone(VALID_PHONE_BOB)
             .withEmail(VALID_EMAIL_BOB).withAddress(VALID_ADDRESS_BOB)
             .withRemark(VALID_REMARK_BOB).withTags(VALID_TAG_MATH, VALID_TAG_SCIENCE)
-            .build();
+            .buildStudent();
 
     public static final String KEYWORD_MATCHING_MEIER = "Meier"; // A keyword that matches MEIER
 
