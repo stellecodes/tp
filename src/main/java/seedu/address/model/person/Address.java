e90947168cc55d1475c807cc9853aee3c074dcac
Xiao-Congchen <xiaocongchen.xcc@gmail.com>
2025-11-03 20:34:54 +0800

    Expand addresses to 300 characters

--- src/main/java/seedu/address/model/person/Address.java
+++ src/main/java/seedu/address/model/person/Address.java
@@ -10,13 +10,13 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Address {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Addresses can take any non-empty value but should not exceed 100 characters";
+            "Addresses can take any non-empty value but should not exceed 300 characters";
 
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[^\\s].{0,99}";
+    public static final String VALIDATION_REGEX = "[^\\s].{0,299}";
 
     public final String value;
 
